854deff148d4dd7fefd2f00b924f866910b0ba25
dheeraj404 <130442066+dheeraj404@users.noreply.github.com>
2024-11-17 12:59:40 +0530

    ss

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL
+  baseURL: 'https://contact-mangement-1sse.onrender.com', // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
